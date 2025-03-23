28e9922eeab254d1abe86882e4608643af38735f
Your Name <yining_pan@mymail.sutd.edu.sg>
2025-03-23 23:51:37 +0800

    misc training scripts updates.

--- 1-train_single.sh
+++ 1-train_single.sh
@@ -7,9 +7,9 @@
 export CUDA_VISIBLE_DEVICES=1
 
 # nohup \
-python -m torch.distributed.launch --nproc_per_node=1 train.py \
--c configs/pa_po_nuscenes_trainval_r50_mini.yaml \
--l nusc_ValDebug_0315_2.log \
+python -m torch.distributed.launch --nproc_per_node=1 train_pie.py \
+-c configs/pa_po_nuscenes_trainval_r50_pie_mini.yaml \
+-l nusc_train_pie_0322.log \
 # -r
 # --local-rank 0
 # > nohup_nusc_20230101_pix_4k2bs_a6000.log 2>&1 &
